7768304302610e3d05b961f6c82fa2d80ec84942
Tom Finke <tom.finke@uni-a.de>
2026-07-22 11:00:20 +0200

    fix[test]: remove port checking, windows and macos allow rebinding to ports

--- test/test_handshake.jl
+++ test/test_handshake.jl
@@ -24,16 +24,11 @@ Preferences.set_preferences!(
     certs_dir="handshake_test_certs"
     @test ~isdir(certs_dir) # fails if certs dir already exists
 
-    port = 8000
-    @test isnothing(close(Sockets.listen(port))) # Test that port is available before server is started
-
     include("../examples/handshake/server.jl")
-    @test_throws Base.IOError Sockets.listen(port) # test that port is now occupied
 
     include("../examples/handshake/client.jl")
 
     close(server)
-    @test isnothing(close(Sockets.listen(port))) # Test that port is available after closing server 
 
     @test isfile("$certs_dir/ca-key.pem")
     @test isfile("$certs_dir/ca.pem")
--- test/test_simple_array_operations.jl
+++ test/test_simple_array_operations.jl
@@ -23,16 +23,11 @@ Preferences.set_preferences!(
 @testset verbose=true showtiming=true "examples/simple_array_operations" begin
     @test ~isdir(certs_dir) # fails if certs dir already exists
     
-    port = 8000
-    @test isnothing(close(Sockets.listen(port))) # Test that port is available before server is started
-
     include("../examples/simple_array_operations/server.jl")
-    @test_throws Base.IOError Sockets.listen(port) # test that port is now occupied
 
     include("../examples/simple_array_operations/client.jl")
 
     close(server)
-    @test isnothing(close(Sockets.listen(port))) # Test that port is available after closing server 
     rm(certs_dir; recursive=true)
 end
 
